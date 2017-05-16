f0191fcfe75eb164d5179477cd4350c5f3e9e183
Tobias <faxmail09@gmx.net>
2017-05-16 19:46:18 +0200

    Added possibility to add conf snippets in files ending in .conf in subdirectory hooks.conf.d

--- liveconfig_lua_hooks/system_hooks.lua
+++ liveconfig_lua_hooks/system_hooks.lua
@@ -5,10 +5,59 @@ local system_hooks      = {}
 
 local hook_directory    = "/usr/share/liveconfig/hooks"
 local hooks_config_file = "/etc/liveconfig/hooks.conf"
+local hooks_config_dir  = "/etc/liveconfig/hooks.conf.d"
 
 local function file_exists(filename)
    local f=io.open(filename,"r")
    if f~=nil then io.close(f) return true end
+end
+
+local function dir_exists(dirname) 
+	res = os.execute("test -d '"..dirname.."'")
+	if(res==0) then return true end
+end
+
+function normalize_dirname(dirname) 
+
+	--[[ 
+
+	"./" 		-> ""
+	"../dirname" 	-> "" (from left)
+	"//" 		-> "/"
+
+	]]--
+	repeat
+		dirname_old=dirname
+		dirname = dirname:gsub("^%./","")
+		dirname = dirname:gsub("([^%.])%./","%1")
+		dirname = dirname:gsub(".%./[^/]+","")
+		dirname = dirname:gsub("//","/")
+	until dirname_old == dirname
+	return dirname
+
+end
+
+function mkdir(dirname)
+	if(dirname) then
+		if(dir_exists(dirname)) then
+			return true
+		end
+		dir_parts={}
+		if(not dirname:match("^/")) then
+			dirname = os.getenv("PWD").."/"..dirname
+		end
+		dirname=normalize_dirname(dirname)
+		dirname:gsub("([^/]+)",function(part) dir_parts[#dir_parts+1]=part end)
+	else
+		return
+	end
+	whole_dir=""
+	for i,part in ipairs(dir_parts) do
+		whole_dir = whole_dir .. "/" .. part
+		if(not dir_exists(whole_dir)) then
+			os.execute(whole_dir)
+		end
+	end
 end
 
 local defined_hooks = {}
@@ -32,35 +81,73 @@ local function hook_validated(hook)
         end
 end
 
+function get_command_output_handle(command)
+	handle,err = io.popen(command,"r")
+	if(handle) then 
+		return handle
+	end
+end
+
+function get_files(dir)
+	
+	local files={}
+	local handle = get_command_output_handle("find "..dir.." -maxdepth 1 -type f")
+	for line in handle:lines() do
+		files[#files+1]=line
+	end
+	handle:close()
+	return files
+end
+
+local function get_hook_files()
+
+	local hook_files = {"/etc/liveconfig/hooks.conf"}
+	if not dir_exists(hooks_config_dir) then
+		mkdir(hooks_config_dir)
+	else
+		for _,filename in ipairs(get_files(hooks_config_dir)) do
+        		LC.log.print(LC.log.INFO,"got filename >>"..filename.."<<")
+			if(filename:match("conf$")) then
+				hook_files[#hook_files+1]=filename
+			end
+		
+		end
+	end
+	return hook_files
+end
+
 local function load_hooks_from_file()
 
-        if(file_exists(hooks_config_file)) then
-                datafile = io.open(hooks_config_file,"r")
-                for line in datafile:lines() do
-                        if(not (line:match("^#") or line:match("^%s*$"))) then
-				local hook = {}
-				line:gsub("^%s*([a-zA-Z0-9_]+)%s*,([a-zA-Z0-9_]+)\.([a-zA-Z0-9_]+)%s*,([a-zA-Z]+)%s*$",
-					function(m1,m2,m3,m4)
-						hook["name"]            = m1
-						hook["lua_module"]      = m2
-						hook["lua_function"]    = m3
-						hook["execution"]       = m4
-					end)
-				LC.log.print(LC.log.INFO,
-					   "name: >>"  ..tostring(hook["name"])         .."<<"
-					 .." mod: >>"  ..tostring(hook["lua_module"])   .."<<"
-					.." func: >>" ..tostring(hook["lua_function"])  .."<<"
-					.." exec: >>"..tostring(hook["execution"])      .."<<")
-				if(hook["name"] ~= nil and hook_validated(hook)) then
-					LC.log.print(LC.log.INFO,"Hook definition for hook "..tostring(hook["name"]).." validated.")
-					defined_hooks[#defined_hooks+1]=hook
-				else
-					LC.log.print(LC.log.WARNING,"Hook definition for hook "..tostring(hook["name"]).." invalid. Ignoring Hook.")
+	for _,hook_file in ipairs(get_hook_files()) do
+        	LC.log.print(LC.log.INFO,"processing hooks file >>"..hook_file.."<<")
+		if(file_exists(hook_file)) then
+			datafile = io.open(hook_file,"r")
+			for line in datafile:lines() do
+				if(not (line:match("^#") or line:match("^%s*$"))) then
+					local hook = {}
+					line:gsub("^%s*([a-zA-Z0-9_]+)%s*,([a-zA-Z0-9_]+)\.([a-zA-Z0-9_]+)%s*,([a-zA-Z]+)%s*$",
+						function(m1,m2,m3,m4)
+							hook["name"]            = m1
+							hook["lua_module"]      = m2
+							hook["lua_function"]    = m3
+							hook["execution"]       = m4
+						end)
+					LC.log.print(LC.log.INFO,
+						   "name: >>"  ..tostring(hook["name"])         .."<<"
+						 .." mod: >>"  ..tostring(hook["lua_module"])   .."<<"
+						.." func: >>" ..tostring(hook["lua_function"])  .."<<"
+						.." exec: >>"..tostring(hook["execution"])      .."<<")
+					if(hook["name"] ~= nil and hook_validated(hook)) then
+						LC.log.print(LC.log.INFO,"Hook definition for hook "..tostring(hook["name"]).." validated.")
+						defined_hooks[#defined_hooks+1]=hook
+					else
+						LC.log.print(LC.log.WARNING,"Hook definition for hook "..tostring(hook["name"]).." invalid. Ignoring Hook.")
+					end
 				end
-                        end
-                end
-
-        end
+			end
+			datafile:close()
+		end
+	end
 end
 
 local function protected_function_load(wanted_mod,wanted_func)
