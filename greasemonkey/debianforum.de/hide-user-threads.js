2bb27134d6636aefe7c3bbfa1cb51eb3424db435
Tobias <faxmail09@gmx.net>
2018-11-14 11:37:04 +0100

    added greasemonkey hide user threads script

---
+++ greasemonkey/debianforum.de/hide-user-threads.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name     DF_Hide_Ignored_User_Threads
+// @author   heisenberg
+// @version  1
+// @grant    none
+// @namespace heisenberg
+// @include /https://debianforum.de/forum/(search|viewforum)\.php/
+// ==/UserScript==
+
+var hide_users = ["BananenBoy","GewaltBrabbler"];
+var debug      = false;
+
+function my_log(msg) {
+  if(debug) {
+     unsafeWindow.console.log(msg);
+  }
+}
+
+var list = document.getElementsByClassName("username");
+
+for (var i = 0; i < list.length; i++) {
+    for(var j = 0; j < hide_users.length; j++) {
+    if (list[i].textContent == hide_users[j]) {
+      var row=list[i].parentElement.parentElement.parentElement.parentElement;
+      my_log("found thread of ignoried user: " +hide_users[j]);
+      row.style.display = "none";
+      break;
+    }
+  }
+}
